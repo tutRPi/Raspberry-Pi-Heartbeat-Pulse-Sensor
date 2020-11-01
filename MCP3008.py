8edae9fd2fbb283beb997e6d39b2e914c8ea7a84
tutRPi <tutRPi@users.noreply.github.com>
2020-11-01 13:19:14 +0100

    Update MCP3008.py

--- MCP3008.py
+++ MCP3008.py
@@ -16,13 +16,18 @@ class MCP3008:
         self.bus, self.device = bus, device
         self.spi = SpiDev()
         self.open()
+        self.spi.max_speed_hz = 1000000 # 1MHz
 
     def open(self):
         self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000 # 1MHz
     
     def read(self, channel = 0):
-        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
-        data = ((adc[1] & 3) << 8) + adc[2]
+        cmd1 = 4 | 2 | (( channel & 4) >> 2)
+        cmd2 = (channel & 3) << 6
+
+        adc = self.spi.xfer2([cmd1, cmd2, 0])
+        data = ((adc[1] & 15) << 8) + adc[2]
         return data
             
     def close(self):
